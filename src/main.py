40029f3c6a7b92dd82c1bceb7bc4a3bc8b557e9d
TakayukiYoshizumi <t.yoshizuminunununu@gmail.com>
2019-04-10 11:24:01 +0900

    01の実装

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
 import processing
 
 processing.p00.processing00().execute("stressed")
+processing.p01.processing01().execute("パタトクカシーー")
--- src/processing/__init__.py
+++ src/processing/__init__.py
@@ -1,2 +1,3 @@
 from .processing_base import processingBase
 from .p00 import processing00
+from .p01 import processing01
