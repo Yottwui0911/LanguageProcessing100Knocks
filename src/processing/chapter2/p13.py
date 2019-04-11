99060f2fa5292ebd2eb90db834049c8c2c101d0b
TakayukiYoshizumi <t.yoshizuminunununu@gmail.com>
2019-04-11 10:36:40 +0900

    パスの変更

--- src/processing/chapter2/p13.py
+++ src/processing/chapter2/p13.py
@@ -5,9 +5,9 @@ class processing13(processingBase):
         super().__init__("p13")
 
     def a_execute(self, input):
-        f1 = open("src/processing/chapter2/col1.txt", "r", encoding="utf-8")
-        f2 = open("src/processing/chapter2/col2.txt", "r", encoding="utf-8")
-        f3 = open("src/processing/chapter2/p13.txt", "w", encoding="utf-8")
+        f1 = open("src/processing/chapter2/result/col1.txt", "r", encoding="utf-8")
+        f2 = open("src/processing/chapter2/result/col2.txt", "r", encoding="utf-8")
+        f3 = open("src/processing/chapter2/result/p13.txt", "w", encoding="utf-8")
         for col1_line, col2_line in zip(f1, f2):
             f3.write(col1_line.rstrip("\n") + '\t' + col2_line)
         f1.close()
