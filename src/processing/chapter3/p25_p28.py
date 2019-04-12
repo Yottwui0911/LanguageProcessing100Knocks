21c3c0b05f56e3e6c2028be0a21565337b36f97d
TakayukiYoshizumi <t.yoshizuminunununu@gmail.com>
2019-04-12 10:25:51 +0900

    28の実装

--- src/processing/chapter3/p25_p28.py
+++ src/processing/chapter3/p25_p28.py
@@ -8,12 +8,14 @@ class processing25_28(processingBase):
     def a_execute(self, input):
         print(self.id + " :")
         match = re.findall('^\{\{基礎情報(.*?)\}\}$', input, re.MULTILINE+re.DOTALL)
-        match2 = re.findall('\|(.*?) = (.*?)\n', match[0])
+        match2 = re.findall('^\|(.*?) = (.*?)$', match[0], re.MULTILINE)
         match_dct = {}
         for t in match2:
             key = re.sub("''+", "" , str(t[0]))
             val = re.sub("''+", "" , str(t[1]))
             key = re.sub(r"\[\[(.*?)([|\|].*)?\]\]", r"\1", str(key), flags=re.MULTILINE)
             val = re.sub(r"\[\[(.*?)([|\|].*)?\]\]", r"\1", str(val), flags=re.MULTILINE)
+            val = re.sub(r"\[.*?\]", r"", str(val), flags=re.MULTILINE) # リンクを殺す
+            val = re.sub(r"\{\{lang\|.*?\|(.*)\}\}", r"\1", str(val), flags=re.MULTILINE) # langの文字部分だけ採用
             match_dct[key] = val
-        print (match_dct)
+        print ("\n".join([key + " : " + val for key,val in match_dct.items()]))
